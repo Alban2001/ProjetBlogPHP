82b362e209dfa7b443c88bd5bd94308cac45d767
Alban Voiriot <alban.voiriot@gmail.com>
2023-05-31 12:23:42 +0200

    Removing of the Mysql username and password from the connection string in the DatabaseConnection file

--- src/Lib/DatabaseConnection.php
+++ src/Lib/DatabaseConnection.php
@@ -4,14 +4,16 @@ namespace Lib;
 
 use PDO;
 
+require_once(__DIR__ . '/../config.php');
+
 class DatabaseConnection
 {
-    public ?PDO $database = null;
+    private ?PDO $database = null;
 
     public function getConnection(): PDO
     {
         if ($this->database === null) {
-            $this->database = new PDO('mysql:host=localhost;dbname=bdd_blogpro;charset=utf8', 'root', 'root');
+            $this->database = new PDO('mysql:host=localhost;dbname=bdd_blogpro;charset=utf8', $_ENV["USERNAME_DB"], $_ENV["PASSWORD_DB"]);
         }
 
         return $this->database;
