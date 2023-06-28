f45c79972e52a2ae273988ea50877eea497d5a7d
Alban Voiriot <alban.voiriot@gmail.com>
2023-06-29 00:31:59 +0200

    Implementation and finishing of the Users Registration Page

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -15,14 +15,48 @@ class UserManager
         $statement = $connection->getConnection()->prepare("SELECT adresse_mail, mot_de_passe FROM utilisateur WHERE adresse_mail = ?");
         $statement->bindParam(1, $adresseMail);
         $statement->execute();
-
         $row = $statement->fetch();
 
         if ((count($row) > 0) && ($adresseMail == $row["adresse_mail"]) && (password_verify($motDePasse, $row["mot_de_passe"]))) {
             return true;
         }
-
         return false;
+    }
+
+    //Permet de vérifier si l'adresse mail est déjà présente dans la BDD
+    public function verifierMail(string $adresseMail): bool
+    {
+        $connection = new DatabaseConnection();
+        $statement = $connection->getConnection()->prepare("SELECT COUNT(adresse_mail) AS 'nbrLigne' FROM utilisateur WHERE adresse_mail = ?");
+        $statement->bindParam(1, $adresseMail);
+        $statement->execute();
+        $row = $statement->fetch();
+
+        if (($row["nbrLigne"] > 0)) {
+            return false;
+        }
+        return true;
+    }
+
+    // Permet d'ajouter un nouvel utilisateur dans la BDD avec le role "user" par défaut
+    // Ici, on va aussi crypté le mot de passe
+    public function add(User $user): void
+    {
+        $nom = $user->getNom();
+        $prenom = $user->getPrenom();
+        $adresseMail = $user->getAdresseMail();
+        $password = password_hash($user->getMotDePasse(), PASSWORD_DEFAULT);
+        $role = $user->getRole();
+
+        $connection = new DatabaseConnection();
+        $statement = $connection->getConnection()->prepare("INSERT INTO utilisateur(nom, prenom, adresse_mail, mot_de_passe, role) VALUES (?, ?, ?, ?, ?)");
+        $statement->bindParam(1, $nom);
+        $statement->bindParam(2, $prenom);
+        $statement->bindParam(3, $adresseMail);
+        $statement->bindParam(4, $password);
+        $statement->bindParam(5, $role);
+
+        $statement->execute();
     }
 
     //Permet d'obtenir toutes les informations d'un utilisateur (id, nom, prenom, adresse mail, mot de passe et rôle)
